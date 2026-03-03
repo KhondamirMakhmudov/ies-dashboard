f5add265afbcee63f2ecaea90e80a5c702ca7124
Khondamir Makhmudov <makhmudovkhondamir@gmail.com>
2026-03-03 12:21:38 +0500

    changed url of config

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export const config = {
-  JAVA_API_URL: "http://10.20.6.30/acs/",
-  PYTHON_API_URL: "http://10.20.6.30/staffio/",
-  GENERAL_AUTH_URL: "http://10.20.6.30",
+  JAVA_API_URL: "https://app.tpp.uz/acs/",
+  PYTHON_API_URL: "https://app.tpp.uz/staffio/",
+  GENERAL_AUTH_URL: "https://app.tpp.uz",
   // GENERAL_AUTH_URL: "http://10.40.9.46:5656",
   // JAVA_API_URL: "http://10.40.9.47:8080/",
   // PYTHON_API_URL: "http://10.40.9.46:1010/",
